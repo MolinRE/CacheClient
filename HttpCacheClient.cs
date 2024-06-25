e6dbc37a2c57638199a0625d06144f4f8e933e20
Комаров Константин <kakomarov@alfabank.ru>
2024-06-26 02:17:27 +0300

    Cache lifetime

--- HttpCacheClient.cs
+++ HttpCacheClient.cs
@@ -9,16 +9,31 @@ public class HttpCacheClient
     private readonly string _dumpDirectory;
     private readonly HttpClient _client;
 
-    public HttpCacheClient(string dumpDirectory)
+    public HttpCacheClient(string dumpDirectory, HttpClient? httpClient = null, int cacheLifeMinutes = 0)
     {
         _dumpDirectory = dumpDirectory;
-        _client = new();
+        _client = httpClient ?? new();
+        
+        if (cacheLifeMinutes > 0 && Directory.Exists(_dumpDirectory))
+        {
+            var di = new DirectoryInfo(_dumpDirectory);
+            var files = di.GetFiles("*", SearchOption.AllDirectories);
+            if (files.Length > 0)
+            {
+                if ((DateTime.Now - files.Max(p => p.CreationTime)).TotalMinutes > cacheLifeMinutes)
+                {
+                    DeleteCache();
+                }
+            }
+        }
     }
 
-    public HttpCacheClient(string dumpDirectory, HttpClient httpClient)
+    public void DeleteCache()
     {
-        _dumpDirectory = dumpDirectory;
-        _client = httpClient;
+        if (Directory.Exists(_dumpDirectory))
+        {
+            Directory.Delete(_dumpDirectory, true);
+        }
     }
 
     public async Task<IHttpResponseMessage> GetAsync(string requestUri, PageFormat format = PageFormat.HTML)
@@ -50,46 +65,5 @@ public class HttpCacheClient
         }
 
         return new CachedResponseMessage(fileName);
-    }
-
-    public async Task<string> GetHtml(string url, string name) => await GetHtml(new Uri(url), name);
-    
-    public async Task<string> GetHtml(Uri url, string name)
-    {
-        var segments = url.AbsolutePath.Split("/", StringSplitOptions.RemoveEmptyEntries);
-        if (segments.All(p => p == string.Empty))
-        {
-            var sb = new StringBuilder();
-            foreach (var ch in name)
-            {
-                sb.Append(Path.GetInvalidFileNameChars().Contains(ch) ? '_' : ch);
-            }
-            
-            segments = new[] { sb.ToString() };
-        }
-        
-        var fileName = Path.Combine(_dumpDirectory, Path.Combine(segments)) + ".html";
-        var dir = Path.GetDirectoryName(fileName)!;
-
-        if (File.Exists(fileName))
-        {
-            return await File.ReadAllTextAsync(fileName, Encoding.UTF8);
-        }
-    
-        #if DEBUG
-        Console.WriteLine("GET " + url.PathAndQuery);
-        #endif
-        
-        var response = await _client.GetAsync(url);
-        await using var stream = await response.Content.ReadAsStreamAsync();
-        using var sr = new StreamReader(stream, Encoding.UTF8);
-        var result = await sr.ReadToEndAsync();
-        if (!Directory.Exists(dir))
-        {
-            Directory.CreateDirectory(dir);
-        }
-        await File.WriteAllTextAsync(fileName, result, Encoding.UTF8);
-
-        return result;
     }
 }
